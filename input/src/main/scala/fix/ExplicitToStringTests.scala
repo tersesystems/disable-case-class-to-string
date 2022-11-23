42e9f5d137cc1c1e2e6620860431da827a6d010b
Will Sargent <will.sargent@gmail.com>
2022-11-23 07:39:34 -0800

    stop collection toString

--- input/src/main/scala/fix/ExplicitToStringTests.scala
+++ input/src/main/scala/fix/ExplicitToStringTests.scala
@@ -22,6 +22,21 @@ object ExplicitToStringTests {
     String.valueOf(foo) // assert: DisableCaseClassToString
   }
 
+  def seqMkString: String = {
+    val foo = Foo("foo")
+    Seq(foo).mkString // assert: DisableCaseClassToString
+  }
+
+  def seqToString: String = {
+    val foo = Foo("foo")
+    Seq(foo).toString // assert: DisableCaseClassToString
+  }
+
+  def mapToString: String = {
+    val foo = Foo("foo")
+    Map("foo" -> foo).toString // assert: DisableCaseClassToString
+  }
+
   def fooMethod: Foo = Foo("foo")
 }
 
